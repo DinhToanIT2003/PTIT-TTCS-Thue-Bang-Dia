bdc30f19f5423845a0590c1e4a33b0af74c217ec
DinhToanIT2003 <trandinhtoan2003@gmail.com>
2024-01-21 11:02:30 +0700

    Add login UI

--- PTIT-TTCS-Thue-Bang-Dia/src/views/Application.java
+++ PTIT-TTCS-Thue-Bang-Dia/src/views/Application.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package views.main;
+package views;
 
 /**
  *
  * @author ACER
  */
-public class Main {
+public class Application {
 
     /**
      * @param args the command line arguments
